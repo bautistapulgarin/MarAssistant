de07d754ab081d8be689b4901b340ce0532615fa
bautistapulgarin <bautistapulgarin@gmail.com>
2025-10-29 11:19:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -155,15 +155,15 @@ st.markdown("""
 </style>
 
 <!-- Fantasmas en la parte derecha (solo arriba → abajo) -->
-<div style="position:fixed; top:0%; right:5%; font-size:30px; opacity:0.2; animation:floatDown 15s linear infinite; z-index:9999;">❄️</div>
-<div style="position:fixed; top:10%; right:7%; font-size:28px; opacity:0.2; animation:floatDown 18s linear infinite; z-index:9999;">❄️</div>
-<div style="position:fixed; top:20%; right:6%; font-size:25px; opacity:0.2; animation:floatDown 16s linear infinite; z-index:9999;">❄️</div>
-<div style="position:fixed; top:25%; right:6%; font-size:25px; opacity:0.2; animation:floatDown 16s linear infinite; z-index:9999;">❄️</div>
+<div style="position:fixed; top:0%; right:5%; font-size:30px; opacity:0.1; animation:floatDown 15s linear infinite; z-index:9999;">❄️</div>
+<div style="position:fixed; top:10%; right:7%; font-size:28px; opacity:0.1; animation:floatDown 18s linear infinite; z-index:9999;">❄️</div>
+<div style="position:fixed; top:20%; right:6%; font-size:25px; opacity:0.1; animation:floatDown 16s linear infinite; z-index:9999;">❄️</div>
+<div style="position:fixed; top:25%; right:6%; font-size:20px; opacity:0.1; animation:floatDown 16s linear infinite; z-index:9999;">❄️</div>
 
 <!-- Calabazas en la parte inferior izquierda (rebote suave) -->
-<div style="position:fixed; bottom:5%; left:48%; font-size:22px; opacity:0.9; animation:floatY 3s ease-in-out infinite; z-index:9999;">🎃</div>
-<div style="position:fixed; bottom:8%; left:50%; font-size:20px; opacity:0.85; animation:floatY 2.8s ease-in-out infinite; z-index:9999;">🎃</div>
-<div style="position:fixed; bottom:6%; left:52%; font-size:18px; opacity:0.9; animation:floatY 3.2s ease-in-out infinite; z-index:9999;">🎃</div>
+<div style="position:fixed; bottom:5%; left:3%; font-size:22px; opacity:0.9; animation:floatY 3s ease-in-out infinite; z-index:9999;">🎃</div>
+<div style="position:fixed; bottom:8%; left:5%; font-size:20px; opacity:0.85; animation:floatY 2.8s ease-in-out infinite; z-index:9999;">🎃</div>
+<div style="position:fixed; bottom:6%; left:7%; font-size:18px; opacity:0.9; animation:floatY 3.2s ease-in-out infinite; z-index:9999;">🎃</div>
 """, unsafe_allow_html=True)
 
 
@@ -484,6 +484,7 @@ st.markdown(
     f"<br><hr><p style='font-size:12px;color:#6b7280;'>Mar Assistant • CONSTRUCTORA MARVAL • Versión: 1.0</p>",
     unsafe_allow_html=True
 )
+
 
 
 
