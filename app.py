6259571027464e3ba7fadf2c470044e91a6c01d7
bautistapulgarin <bautistapulgarin@gmail.com>
2025-11-13 08:54:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -521,6 +521,7 @@ st.markdown("""
 <div style="position:fixed; bottom:8%; left:10%; font-size:25px; opacity:1; animation:floatY 2.8s ease-in-out infinite; z-index:9999;">🦌</div>
 <div style="position:fixed; bottom:6%; left:12%; font-size:30px; opacity:1; animation:floatY 3.2s ease-in-out infinite; z-index:9999;">🦌</div>
 <div style="position:fixed; bottom:6%; left:16%; font-size:30px; opacity:1; animation:floatY 0s ease-in-out infinite; z-index:9999;">🛷</div>
+<div style="position:fixed; bottom:6.5%; left:16%; font-size:30px; opacity:1; animation:floatY 0s ease-in-out infinite; z-index:9999;">🎅🏻</div>
 """, unsafe_allow_html=True)
 
 # -----------------------------
@@ -1751,6 +1752,7 @@ elif st.session_state.current_view == 'chat':
                 st.error(titulo)
     
     st.markdown("<div style='height: 100px;'></div>", unsafe_allow_html=True)
+
 
 
 
