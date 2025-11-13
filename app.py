9fe6350b8e36eea3ac8165ddd1823706ec06ace8
bautistapulgarin <bautistapulgarin@gmail.com>
2025-11-13 08:49:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -509,17 +509,17 @@ st.markdown("""
 }
 </style>
 
-<div style="position:fixed; top:0%; right:5%; font-size:30px; opacity:0.1; animation:floatDown 15s linear infinite; z-index:9999;">❄️</div>
-<div style="position:fixed; top:10%; right:7%; font-size:28px; opacity:0.1; animation:floatDown 18s linear infinite; z-index:9999;">❄️</div>
-<div style="position:fixed; top:20%; right:6%; font-size:25px; opacity:0.1; animation:floatDown 16s linear infinite; z-index:9999;">❄️</div>
-<div style="position:fixed; top:25%; right:8%; font-size:20px; opacity:0.1; animation:floatDown 15s linear infinite; z-index:9999;">❄️</div>
-<div style="position:fixed; top:10%; right:5%; font-size:28px; opacity:0.1; animation:floatDown 13s linear infinite; z-index:9999;">❄️</div>
-<div style="position:fixed; top:20%; right:7%; font-size:25px; opacity:0.1; animation:floatDown 15s linear infinite; z-index:9999;">❄️</div>
-<div style="position:fixed; top:25%; right:9%; font-size:20px; opacity:0.1; animation:floatDown 11s linear infinite; z-index:9999;">❄️</div>
+<div style="position:fixed; top:0%; right:5%; font-size:30px; opacity:0.8; animation:floatDown 15s linear infinite; z-index:9999;">❄️</div>
+<div style="position:fixed; top:10%; right:7%; font-size:28px; opacity:0.8; animation:floatDown 18s linear infinite; z-index:9999;">❄️</div>
+<div style="position:fixed; top:20%; right:6%; font-size:25px; opacity:0.8; animation:floatDown 16s linear infinite; z-index:9999;">❄️</div>
+<div style="position:fixed; top:25%; right:8%; font-size:20px; opacity:0.8; animation:floatDown 15s linear infinite; z-index:9999;">❄️</div>
+<div style="position:fixed; top:10%; right:5%; font-size:28px; opacity:0.8; animation:floatDown 13s linear infinite; z-index:9999;">❄️</div>
+<div style="position:fixed; top:20%; right:7%; font-size:25px; opacity:0.8; animation:floatDown 15s linear infinite; z-index:9999;">❄️</div>
+<div style="position:fixed; top:25%; right:9%; font-size:20px; opacity:0.8; animation:floatDown 11s linear infinite; z-index:9999;">❄️</div>
 
-<div style="position:fixed; bottom:5%; left:8%; font-size:22px; opacity:1; animation:floatY 3s ease-in-out infinite; z-index:9999;">🎅🏻</div>
-<div style="position:fixed; bottom:8%; left:10%; font-size:20px; opacity:1; animation:floatY 2.8s ease-in-out infinite; z-index:9999;">🤶🏻</div>
-<div style="position:fixed; bottom:6%; left:12%; font-size:18px; opacity:1; animation:floatY 3.2s ease-in-out infinite; z-index:9999;">🎄</div>
+<div style="position:fixed; bottom:5%; left:8%; font-size:30px; opacity:1; animation:floatY 3s ease-in-out infinite; z-index:9999;">🦌</div>
+<div style="position:fixed; bottom:8%; left:10%; font-size:30px; opacity:1; animation:floatY 2.8s ease-in-out infinite; z-index:9999;">🦌</div>
+<div style="position:fixed; bottom:6%; left:12%; font-size:45px; opacity:1; animation:floatY 3.2s ease-in-out infinite; z-index:9999;">🎅🏻</div>
 """, unsafe_allow_html=True)
 
 # -----------------------------
@@ -1750,6 +1750,7 @@ elif st.session_state.current_view == 'chat':
                 st.error(titulo)
     
     st.markdown("<div style='height: 100px;'></div>", unsafe_allow_html=True)
+
 
 
 
